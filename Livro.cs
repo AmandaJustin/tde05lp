d01dbcf2a6edae5ce5bc0db2c6974cd46f2f0e60
AmandaJustin <amandajustindelima13@gmail.com>
2024-09-12 21:21:08 -0300

    first commit

---
+++ Livro.cs
@@ -0,0 +1,21 @@
+class Livro{
+
+    public string titulo;
+
+    public string autor;
+
+    public int ano;
+
+    public int paginas;
+
+    public void Informacoes()
+    {
+        Console.WriteLine($"Autor: {autor}\nTitulo: {titulo}\nAno: {ano}\nPaginas: {paginas}");
+    }
+
+    public bool TemMaisDe300Paginas()
+    {
+       return paginas > 300;
+     
+    }
+}
